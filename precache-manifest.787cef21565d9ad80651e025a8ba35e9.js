87906dc8fa23e5509426608f6d0b10fbdd115998
Jonathan Montiel <33934371+JohnMon323@users.noreply.github.com>
2020-09-14 23:10:00 -0500

    Updates

---
+++ precache-manifest.787cef21565d9ad80651e025a8ba35e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92ac7e4f56c778e1716d144942aba84",
+    "url": "/tarjeta/index.html"
+  },
+  {
+    "revision": "954344e18d788291aaa8",
+    "url": "/tarjeta/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "317ef367df95af003bcb",
+    "url": "/tarjeta/static/css/main.e747e2b6.chunk.css"
+  },
+  {
+    "revision": "954344e18d788291aaa8",
+    "url": "/tarjeta/static/js/2.2bd54c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarjeta/static/js/2.2bd54c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317ef367df95af003bcb",
+    "url": "/tarjeta/static/js/main.957225e6.chunk.js"
+  },
+  {
+    "revision": "288a8df668e836922228",
+    "url": "/tarjeta/static/js/runtime-main.36583d29.js"
+  }
+]);
